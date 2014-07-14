0c391a93f5f3131cad4dc5c9c7ea8426d52f1d8f
david.shrader <david.shrader@spottradingllc.com>
2014-07-14 13:28:02 -0500

    Initial commit

--- web/zoom/handlers/get_application_dependencies_handler.py
+++ web/zoom/handlers/get_application_dependencies_handler.py
@@ -15,6 +15,12 @@ class GetApplicationDependenciesHandler(tornado.web.RequestHandler):
         # get dependencies
         # parse configuration path from get request
         configurationPath = self.get_argument("configurationPath")
+
+        # get agent name from configuration path
+        serviceName = configurationPath[::-1]
+        serviceName = serviceName[:serviceName.index("/")][::-1]
+
+        #parse application host from configuration path
         server = self.get_argument("applicationHost")
 
         server = server.upper()
@@ -25,7 +31,14 @@ class GetApplicationDependenciesHandler(tornado.web.RequestHandler):
             data, stat = self.zk.get(path)
 
             root = ET.fromstring(data)
-            startAction = root.find('Automation/Component/Actions/Action[@id="start"]')
+
+            # get correct component within configuration
+            if(root.find('Automation/Component[@id="' + serviceName + '"]') is not None):
+                component = root.find('Automation/Component[@id="' + serviceName + '"]')
+            else:
+                component = root.find('Automation/Component[@registrationpath="' + configurationPath + '"]')
+
+            startAction = component.find('Actions/Action[@id="start"]')
 
             dependencies = []
             for predicate in startAction.iter('Predicate'):
