974eca70b51ed3f00fcabeee1a0b2f2f40a4c9f5
Carl Kershaw <carl.kershaw@spottrading.com>
2014-08-13 16:37:17 -0500

    moved faqs

--- client/viewmodels/navbar.js
+++ client/viewmodels/navbar.js
@@ -8,8 +8,8 @@
             router.map([
                 { route: '', title: "Application State", moduleId: 'viewmodels/application_state', nav: true },
                 { route: 'config', title: "Server Config", moduleId: 'viewmodels/server_config', nav: true },
-                { route: 'appFAQ', title: "Application State FAQ", moduleId: 'viewmodels/application_state_faq', nav: true },
-                { route: 'configFAQ', title: "Server Config FAQ", moduleId: 'viewmodels/server_config_faq', nav: true }
+                { route: 'appFAQ', title: "Application State FAQ", moduleId: 'viewmodels/faq/application_state', nav: true },
+                { route: 'configFAQ', title: "Server Config FAQ", moduleId: 'viewmodels/faq/server_config', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
