f6c2b28557718bbbe33a88f94669682bcef079b5
Carl Kershaw <carl.kershaw@spottrading.com>
2014-08-13 14:26:42 -0500

    Font-Awesome loading, cleanup

--- client/viewmodels/navbar.js
+++ client/viewmodels/navbar.js
@@ -7,7 +7,9 @@
         activate: function () {
             router.map([
                 { route: '', title: "Application State", moduleId: 'viewmodels/application_state', nav: true },
-                { route: 'config', title: "Server Config", moduleId: 'viewmodels/server_config', nav: true }
+                { route: 'config', title: "Server Config", moduleId: 'viewmodels/server_config', nav: true },
+                { route: 'appFAQ', title: "Application State FAQ", moduleId: 'viewmodels/application_state_faq', nav: true },
+                { route: 'configFAQ', title: "Server Config FAQ", moduleId: 'viewmodels/server_config_faq', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
