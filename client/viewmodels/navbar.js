ac32026adce6e0e028d9f6140c9baaa10d490255
Carl Kershaw <carl.kershaw@spottrading.com>
2014-08-15 12:25:13 -0500

    moved the faqs to a dropdown, but can't figure out active styling

--- client/viewmodels/navbar.js
+++ client/viewmodels/navbar.js
@@ -6,9 +6,10 @@
         login: login,
         activate: function () {
             router.map([
+                //If you want to add a route, make sure to update the indices in navbar.html...
                 { route: '', title: "Application State", moduleId: 'viewmodels/application_state', nav: true },
                 { route: 'config', title: "Server Config", moduleId: 'viewmodels/server_config', nav: true },
-                { route: 'appFAQ', title: "Application State FAQ", moduleId: 'viewmodels/faq/application_state', nav: true },
+                { route: 'appFAQ', title: "App State FAQ", moduleId: 'viewmodels/faq/application_state', nav: true },
                 { route: 'configFAQ', title: "Server Config FAQ", moduleId: 'viewmodels/faq/server_config', nav: true }
             ]).buildNavigationModel();
             
