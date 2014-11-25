acba61773147dfa28f69608e0b309dc1e0083a00
Jack Miner <jack.miner@spottradingllc.com>
2014-11-25 14:20:55 -0600

    added handler file

---
+++ server/zoom/www/handlers/list_pillar_servers_handler.py
@@ -0,0 +1,25 @@
+import tornado.ioloop
+import tornado.web
+import logging
+import json
+
+from spot.zoom.common.decorators import TimeThis
+
+
+class ListPillarServersHandler(tornado.web.RequestHandler):
+    @property
+    def zk(self):
+        """
+        :rtype: spot.zoom.www.zoo_keeper.ZooKeeper
+        """
+        return self.application.zk
+
+    @TimeThis(__file__)
+    def get(self):
+        logging.info("Generating list of servers")
+        print 'generating list of servers'
+        # get all nodes at the root config path
+        path = self.application.configuration.pillar_path
+        logging.info(path)
+        nodes = self.zk.get_children(path)
+        self.write(json.dumps(nodes))
