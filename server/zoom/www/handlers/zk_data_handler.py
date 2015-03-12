c2026f01a43466c04111ade1b87ff9475e94fb15
Nick Moskwinski <nick.moskwinski@spottradingllc.com>
2015-03-12 15:10:45 -0500

    normalize api returns for /api/zookeeper

--- server/zoom/www/handlers/zk_data_handler.py
+++ server/zoom/www/handlers/zk_data_handler.py
@@ -5,7 +5,6 @@ import tornado.ioloop
 import tornado.web
 
 from kazoo.exceptions import (
-    ZookeeperError,
     NotEmptyError,
     NodeExistsError,
     NoNodeError
@@ -22,34 +21,57 @@ class ZooKeeperDataHandler(tornado.web.RequestHandler):
         return self.application.zk
 
     @TimeThis(__file__)
-    def put(self, path):
-        """
-        Set data on a Zookeeper path
-        :type path: str
-        """
-        logging.info('Client {0} changing data for path: {1}'
-                     .format(self.request.remote_ip, path))
-        if self.zk.exists(path):
-            self.zk.set(path, value=self.request.body)
-            data, stat = self.zk.get(path)
-            self.write(data)
-        else:
-            self.write('Path does not exist: {0}'.format(path))
-        logging.info(path)
-
-    @TimeThis(__file__)
     def get(self, path):
         """
         Get data on a Zookeeper path
         :type path: str
         """
-        logging.info('Getting data for path: {0}'.format(path))
+        ret = {"method": 'GET', "path": path, "code": httplib.OK,
+               "data": None, "error": None}
+        logging.debug('Getting data for path: {0}'.format(path))
         if self.zk.exists(path):
             data, stat = self.zk.get(path)
-            self.write(data)
+            ret['data'] = data
+            self.write(ret)
         else:
-            self.write('Path does not exist: {0}'.format(path))
-            self.set_status(httplib.NO_CONTENT)
+            ret['code'] = httplib.NOT_FOUND
+            ret['error'] = 'Path does not exist.'
+            self.write(ret)
+            self.set_status(ret['code'])
+
+    @TimeThis(__file__)
+    def put(self, path):
+        """
+        Set data on a Zookeeper path
+        :type path: str
+        """
+        ret = {"method": 'PUT', "path": path, "code": httplib.OK,
+               "data": None, "error": None}
+        logging.debug('Client {0} changing data for path: {1}'
+                      .format(self.request.remote_ip, path))
+        if self.zk.exists(path):
+            self.zk.set(path, value=self.request.body)
+            self.write(ret)
+        else:
+            ret['code'] = httplib.NOT_FOUND
+            ret['error'] = 'Path does not exist.'
+            self.write(ret)
+            self.set_status(ret['code'])
+
+    @TimeThis(__file__)
+    def head(self, path):
+        """
+        Return whether the path exists in Zookeeper
+        :type path: str
+        """
+        ret = {"method": 'HEAD', "path": path, "code": httplib.OK,
+               "data": None, "error": None}
+        if self.zk.exists(path):
+            self.write(ret)
+        else:
+            ret['code'] = httplib.NOT_FOUND
+            self.write(ret)
+            self.set_status(ret['code'])
 
     @TimeThis(__file__)
     def post(self, path):
@@ -57,74 +79,76 @@ class ZooKeeperDataHandler(tornado.web.RequestHandler):
         Create Zookeeper node
         :type path: str
         """
+        ret = {"method": 'POST', "path": path, "code": httplib.OK,
+               "data": None, "error": None}
         try:
-            user = self.get_argument('username', default=None)
-            makepath = self._check_bool_str(
-                self.get_argument('makepath', default='False'))
             data = self.get_argument('data', '')
 
-            if user is None:
-                self.write("You must provide a username")
-                self.set_status(httplib.BAD_REQUEST)
-                return
-
-            self.zk.create(path, value=json.dumps(data), makepath=makepath)
-            logging.info('Path created. User={0}, path={1}'.format(user, path))
-            self.set_status(httplib.CREATED)
-
-        except NoNodeError:
-            self.write('Path base does not exist for path: {0}'.format(path))
-            self.set_status(httplib.REQUESTED_RANGE_NOT_SATISFIABLE)
+            self.zk.create(path, value=json.dumps(data), makepath=True)
+            logging.debug('Path created. path={1}'.format(path))
+            ret['code'] = httplib.CREATED
+            self.write(ret)
+            self.set_status(ret['code'])
 
         except NodeExistsError:
-            self.write('Path at {0} already exists.'.format(path))
-            self.set_status(httplib.FORBIDDEN)
+            ret['error'] = 'Path already exists.'
+            ret['code'] = httplib.CONFLICT
+            self.write(ret)
+            self.set_status(ret['code'])
 
         except Exception as e:
             logging.exception('Unhandled Exception.')
-            self.write('Unhandled Exception occurred: {0}'.format(e))
-            self.set_status(httplib.INTERNAL_SERVER_ERROR)
+            ret['error'] = 'Unhandled Exception occurred: {0}'.format(e)
+            ret['code'] = httplib.INTERNAL_SERVER_ERROR
+            self.write(ret)
+            self.set_status(ret['code'])
 
     @TimeThis(__file__)
     def delete(self, path):
         """
         Uses custom header in the form:
-            {'username': 'john.smith', 'recurse': 'False'}
+            {'recurse': 'False'}
         :type path: str
         """
-        recurse = self._check_bool_str(self.request.headers.get('recurse'))
-        user = self.request.headers.get('username')
-
-        if user is None:
-            self.write("You must provide a username")
-            self.set_status(httplib.BAD_REQUEST)
-            return
+        ret = {"method": 'PUT', "path": path, "code": httplib.OK,
+               "data": None, "error": None}
+        recurse = self._check_bool_str(self.request.headers.get('recurse', 'False'))
 
         if recurse is None:
-            self.write("Please provide recurse as either True or False")
-            self.set_status(httplib.BAD_REQUEST)
+            ret['error'] = "Please provide 'recurse' as a custom header. " \
+                           "E.g., curl -H 'recurse: True' "
+            ret['code'] = httplib.BAD_REQUEST
+            self.write(ret)
+            self.set_status(ret['code'])
             return
 
         try:
             self.zk.delete(path, recursive=recurse)
-            logging.info("User {0} initiated delete. Path={1}, "
-                         "recursive={2}".format(user, path, recurse))
+            logging.info("Delete initiated. Path={0}, recursive={1}"
+                         .format(path, recurse))
+            self.write(ret)
 
         except NoNodeError:
-            self.write("Failed, path does not exist: {0}".format(path))
-            self.set_status(httplib.REQUESTED_RANGE_NOT_SATISFIABLE)
+            ret['code'] = httplib.NOT_FOUND
+            ret['error'] = 'Path does not exist.'
+            self.write(ret)
+            self.set_status(ret['code'])
+
         except NotEmptyError:
-            self.write("Please make sure to specify with no subdirectories,"
-                       " or delete recursively by setting header 'recurse' "
-                       "to True.\n")
-            self.set_status(httplib.FORBIDDEN)
-        except ZookeeperError as e:
-            self.write("Zookeeper error occured while processing request. "
-                       "Error number: {0}".format(e))
-            self.set_status(httplib.INTERNAL_SERVER_ERROR)
+            ret['error'] = (
+                "Please make sure to specify a path with no subdirectories, or "
+                "delete recursively by setting 'recurse' as a custom header. "
+                "E.g., curl -H 'recurse: True' ")
+            ret['code'] = httplib.CONFLICT
+            self.write(ret)
+            self.set_status(ret['code'])
+
         except Exception as e:
-            self.write("Unexpected error occured: {0}".format(e))
-            self.set_status(httplib.INTERNAL_SERVER_ERROR)
+            logging.exception('Unhandled Exception.')
+            ret['error'] = 'Unhandled Exception occurred: {0}'.format(e)
+            ret['code'] = httplib.INTERNAL_SERVER_ERROR
+            self.write(ret)
+            self.set_status(ret['code'])
 
     def _check_bool_str(self, string):
         if string is None:
