35dd94228e89bd632ec3373d318415f18080e823
Nick Moskwinski <nick.moskwinski@spottradingllc.com>
2015-03-19 10:51:10 -0500

    commented out a lot of dependency code ahead of moving d3 stuff to be standalone

--- client/classes/dependency-maps/DependencyMaps.js
+++ client/classes/dependency-maps/DependencyMaps.js
@@ -16,57 +16,57 @@ define([
         });
 
         self.createDependentsDict = function(appState) {
-            var name = appState.configurationPath.substring(appState.configurationPath.indexOf('application/') + 'application/'.length);
-            if (appState.dependencyModel.requiredBy().length === 0) {
-                return {
-                    name: name,
-                    status: appState.applicationStatus(),
-                    grayed: appState.grayed(),
-                    size: appState.dependencyModel.requiredBy().length + 1,
-                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
-                };
-            }
-            else {
-                return {
-                    name: name,
-                    children: ko.utils.arrayMap(appState.dependencyModel.requiredBy(), function(dependent) {
-                        return self.createDependentsDict(dependent);
-                    }),
-                    grayed: appState.grayed(),
-                    status: appState.applicationStatus(),
-                    size: appState.dependencyModel.requiredBy().length + 1,
-                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
-                };
-            }
+//            var name = appState.configurationPath.substring(appState.configurationPath.indexOf('application/') + 'application/'.length);
+//            if (appState.dependencyModel.requiredBy().length === 0) {
+//                return {
+//                    name: name,
+//                    status: appState.applicationStatus(),
+//                    grayed: appState.grayed(),
+//                    size: appState.dependencyModel.requiredBy().length + 1,
+//                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
+//                };
+//            }
+//            else {
+//                return {
+//                    name: name,
+//                    children: ko.utils.arrayMap(appState.dependencyModel.requiredBy(), function(dependent) {
+//                        return self.createDependentsDict(dependent);
+//                    }),
+//                    grayed: appState.grayed(),
+//                    status: appState.applicationStatus(),
+//                    size: appState.dependencyModel.requiredBy().length + 1,
+//                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
+//                };
+//            }
         };
 
         self.createRequirementsDict = function(appState) {
-            var name = appState.configurationPath.substring(appState.configurationPath.indexOf('application/') + 'application/'.length);
-            // This is a fake app state created from outside the ../state/application tree
-            // (doesn't have all the needed attributes of a real state)
-            if (typeof appState.dependencyModel === 'undefined') {
-                return {
-                    name: name,
-                    status: appState.applicationStatus(),
-                    grayed: false,
-                    size: 1,
-                    errorState: appState.errorState(),
-                    timeComponent: false
-                };
-            }
-            else {
-                return {
-                    name: name,
-                    children: ko.utils.arrayMap(appState.dependencyModel.requires(), function(requirement) {
-                        return self.createRequirementsDict(requirement.state);
-                    }),
-                    status: appState.applicationStatus(),
-                    grayed: appState.grayed(),
-                    size: appState.dependencyModel.requires().length + 1,
-                    errorState: appState.errorState(),
-                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
-                };
-            }
+//            var name = appState.configurationPath.substring(appState.configurationPath.indexOf('application/') + 'application/'.length);
+//            // This is a fake app state created from outside the ../state/application tree
+//            // (doesn't have all the needed attributes of a real state)
+//            if (typeof appState.dependencyModel === 'undefined') {
+//                return {
+//                    name: name,
+//                    status: appState.applicationStatus(),
+//                    grayed: false,
+//                    size: 1,
+//                    errorState: appState.errorState(),
+//                    timeComponent: false
+//                };
+//            }
+//            else {
+//                return {
+//                    name: name,
+//                    children: ko.utils.arrayMap(appState.dependencyModel.requires(), function(requirement) {
+//                        return self.createRequirementsDict(requirement.state);
+//                    }),
+//                    status: appState.applicationStatus(),
+//                    grayed: appState.grayed(),
+//                    size: appState.dependencyModel.requires().length + 1,
+//                    errorState: appState.errorState(),
+//                    timeComponent: (appState.dependencyModel.zookeepergooduntiltime().length > 0)
+//                };
+//            }
         };
 
         // check if some appState's dependents contain a particular appState
@@ -103,75 +103,75 @@ define([
         };
 
         self.dependents = ko.computed(function() {
-            var dict = ko.observableArray([]);
-
-            // sort app states based on number of requirements
-            var sortedAppStates = self.applicationStateArray().slice().sort(function(left, right) {
-                if (left.grayed()) { return 1; } // if app is disabled, push to bottom
-                else if (left.dependencyModel.requiredBy().length === right.dependencyModel.requiredBy().length) {
-                    return 0;
-                }
-                else if (left.dependencyModel.requiredBy().length < right.dependencyModel.requiredBy().length) {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            });
-
-            // filter the sorted app states so no dependencies are included more than once
-            var filteredAppStates = ko.observableArray([]);
-            ko.utils.arrayForEach(sortedAppStates, function(appState) {
-                // include no app state at some level if it is included at some deeper level
-                if (!self.arrayDependentsContainAppState(filteredAppStates(), appState)) {
-                    filteredAppStates.push(appState);
-                }
-            });
-
-            // create the dependents dictionary for each dependence relationship
-            ko.utils.arrayForEach(filteredAppStates(), function(appState) {
-                dict.push(self.createDependentsDict(appState));
-            });
-
-            return {name: 'Zoom', children: dict.slice(), status: 'running', size: dict.length + 1};
+//            var dict = ko.observableArray([]);
+//
+//            // sort app states based on number of requirements
+//            var sortedAppStates = self.applicationStateArray().slice().sort(function(left, right) {
+//                if (left.grayed()) { return 1; } // if app is disabled, push to bottom
+//                else if (left.dependencyModel.requiredBy().length === right.dependencyModel.requiredBy().length) {
+//                    return 0;
+//                }
+//                else if (left.dependencyModel.requiredBy().length < right.dependencyModel.requiredBy().length) {
+//                    return 1;
+//                }
+//                else {
+//                    return -1;
+//                }
+//            });
+//
+//            // filter the sorted app states so no dependencies are included more than once
+//            var filteredAppStates = ko.observableArray([]);
+//            ko.utils.arrayForEach(sortedAppStates, function(appState) {
+//                // include no app state at some level if it is included at some deeper level
+//                if (!self.arrayDependentsContainAppState(filteredAppStates(), appState)) {
+//                    filteredAppStates.push(appState);
+//                }
+//            });
+//
+//            // create the dependents dictionary for each dependence relationship
+//            ko.utils.arrayForEach(filteredAppStates(), function(appState) {
+//                dict.push(self.createDependentsDict(appState));
+//            });
+//
+//            return {name: 'Zoom', children: dict.slice(), status: 'running', size: dict.length + 1};
         }).extend({rateLimit: 2000});
 
         self.requirements = ko.computed(function() {
             var dict = ko.observableArray([]);
-
-            // sort app states based on status, then whether or not their requirements are up, then alphabetically
-            var sortedAppStates = self.applicationStateArray().slice().sort(function(left, right) {
-                if (left.grayed()) { return 1; } // if app is disabled, push to bottom
-                else if (left.applicationStatus() === right.applicationStatus()) {
-                    if (left.dependencyModel.requirementsAreUp() === right.dependencyModel.requirementsAreUp()) {
-                        if (left.configurationPath.toLowerCase() < right.configurationPath.toLowerCase()) {
-                            return -1;
-                        }
-                        else if (left.configurationPath.toLowerCase() > right.configurationPath.toLowerCase()) {
-                            return 1;
-                        }
-                        else {
-                            return 0;
-                        }
-                    }
-                    else if (left.dependencyModel.requirementsAreUp() && !right.dependencyModel.requirementsAreUp()) {
-                        return 1;
-                    }
-                    else {
-                        return -1;
-                    }
-                }
-                else if (left.applicationStatus() === 'running') {
-                    return 1;
-                }
-                else {
-                    return -1;
-                }
-            });
-
-            ko.utils.arrayForEach(sortedAppStates, function(appState) {
-                dict.push(self.createRequirementsDict(appState));
-            });
+//
+//            // sort app states based on status, then whether or not their requirements are up, then alphabetically
+//            var sortedAppStates = self.applicationStateArray().slice().sort(function(left, right) {
+//                if (left.grayed()) { return 1; } // if app is disabled, push to bottom
+//                else if (left.applicationStatus() === right.applicationStatus()) {
+//                    if (left.dependencyModel.requirementsAreUp() === right.dependencyModel.requirementsAreUp()) {
+//                        if (left.configurationPath.toLowerCase() < right.configurationPath.toLowerCase()) {
+//                            return -1;
+//                        }
+//                        else if (left.configurationPath.toLowerCase() > right.configurationPath.toLowerCase()) {
+//                            return 1;
+//                        }
+//                        else {
+//                            return 0;
+//                        }
+//                    }
+//                    else if (left.dependencyModel.requirementsAreUp() && !right.dependencyModel.requirementsAreUp()) {
+//                        return 1;
+//                    }
+//                    else {
+//                        return -1;
+//                    }
+//                }
+//                else if (left.applicationStatus() === 'running') {
+//                    return 1;
+//                }
+//                else {
+//                    return -1;
+//                }
+//            });
+//
+//            ko.utils.arrayForEach(sortedAppStates, function(appState) {
+//                dict.push(self.createRequirementsDict(appState));
+//            });
 
             return {name: 'Zoom', children: dict.slice(), status: 'running', errorState: 'ok'};
         }).extend({rateLimit: 1000});
@@ -209,8 +209,8 @@ define([
             self.dependencyDrawer.inputJSON(newDict);
         });
 
-        self.dependents.subscribe(function(newDict) {
-            self.partitionChart.inputJSON(newDict);
-        });
+//        self.dependents.subscribe(function(newDict) {
+//            self.partitionChart.inputJSON(newDict);
+//        });
     };
 });
--- client/model/dependencyModel.js
+++ client/model/dependencyModel.js
@@ -7,6 +7,7 @@ define(['knockout', 'model/constants'], function(ko, constants) {
         self.weekend = ko.observableArray([]).extend({rateLimit: 2000});
         self.zookeepergooduntiltime = ko.observableArray([]).extend({rateLimit: 2000});
         self.requires = ko.observableArray([]).extend({rateLimit: 2000});
+        self.newRequires = ko.observableArray([]).extend({rateLimit: 2000});
         self.showDependencies = ko.observable(false);
 
         // maps a predicate type to an observable array
@@ -25,109 +26,110 @@ define(['knockout', 'model/constants'], function(ko, constants) {
         };
 
         self.handleUpdate = function(update) {
-            parentAppState.mtime = Date.now();
-            if (parentAppState.applicationHost() === '') { return; }
-
-            // clear all dependencies
-            for (var key in arrayMapping) {
-                if (arrayMapping.hasOwnProperty(key)) {
-                    arrayMapping[key].removeAll();
-                }
-            }
-
-            update.dependencies.forEach(function(entry) {
-                var neverFound = true;
-                var predType = entry.type;
-                var path = entry.path;
-                var operational = entry.operational;
-
-                // if path = /foo, match both /foo/bar and /foo/baz
-                if (predType === constants.predicateTypes.ZooKeeperHasGrandChildren) {
-                    ko.utils.arrayForEach(applicationStateArray(), function(applicationState) {
-                        if (applicationState.configurationPath.substring(0, path.length) === path) {
-                            self.requires.push({'state': applicationState, 'operational': operational});
-                            neverFound = false;
-                        }
-                    });
-                }
-                else if (predType === constants.predicateTypes.ZooKeeperHasChildren) {
-                    var applicationState = ko.utils.arrayFirst(applicationStateArray(), function(applicationState) {
-                        return (path === applicationState.configurationPath);
-                    });
-                    if (applicationState) {
-                        neverFound = false;
-                        self.requires.push({'state': applicationState, 'operational': operational});
-                    }
-                }
-                // if this predicate type exists in arrayMapping
-                else if (typeof arrayMapping[predType] !== 'undefined') {
-                    // push path to appropriate array based on predicate type
-                    arrayMapping[predType].push({'state': path, 'operational': operational});
-                    neverFound = false;
-                }
-                else {
-                    neverFound = false;
-                }
-
-                // Since the server only gets applications located in /spot/software/state/application,
-                // this handles applications outside of that path that were added in the configuration
-                if (neverFound) {
-                    // 'simulate' an applicationState object - /classes/DependencyMaps and this file rely
-                    // on having these attributes, at a minimum. We just want to display the missing app
-                    // so this should be OK - but not ideal
-                    var showAsMissing = {
-                        'componentId': path,
-                        'configurationPath': path,
-                        'applicationStatusClass': constants.glyphs.unknownQMark,
-                        'applicationStatusBg': constants.colors.unknownGray,
-                        'applicationStatus': ko.observable(constants.applicationStatuses.unknown),
-                        'predType': predType,
-                        'requires': ko.observableArray([]),
-                        'errorState': ko.observable(constants.errorStates.unknown)
-                    };
-                    self.requires.push({'state': showAsMissing, 'operational': operational});
-                }
-            });
+//            parentAppState.mtime = Date.now();
+//            if (parentAppState.applicationHost() === '') { return; }
+//
+//            // clear all dependencies
+//            for (var key in arrayMapping) {
+//                if (arrayMapping.hasOwnProperty(key)) {
+//                    arrayMapping[key].removeAll();
+//                }
+//            }
+//
+//            update.dependencies.forEach(function(entry) {
+//                var neverFound = true;
+//                var predType = entry.type;
+//                var path = entry.path;
+//                var operational = entry.operational;
+//
+//                // if path = /foo, match both /foo/bar and /foo/baz
+//                if (predType === constants.predicateTypes.ZooKeeperHasGrandChildren) {
+//                    ko.utils.arrayForEach(applicationStateArray(), function(applicationState) {
+//                        if (applicationState.configurationPath.substring(0, path.length) === path) {
+//                            self.requires.push({'state': applicationState, 'operational': operational});
+//                            neverFound = false;
+//                        }
+//                    });
+//                }
+//                else if (predType === constants.predicateTypes.ZooKeeperHasChildren) {
+//                    var applicationState = ko.utils.arrayFirst(applicationStateArray(), function(applicationState) {
+//                        return (path === applicationState.configurationPath);
+//                    });
+//                    if (applicationState) {
+//                        neverFound = false;
+//                        self.requires.push({'state': applicationState, 'operational': operational});
+//                    }
+//                }
+//                // if this predicate type exists in arrayMapping
+//                else if (typeof arrayMapping[predType] !== 'undefined') {
+//                    // push path to appropriate array based on predicate type
+//                    arrayMapping[predType].push({'state': path, 'operational': operational});
+//                    neverFound = false;
+//                }
+//                else {
+//                    neverFound = false;
+//                }
+//
+//                // Since the server only gets applications located in /spot/software/state/application,
+//                // this handles applications outside of that path that were added in the configuration
+//                if (neverFound) {
+//                    // 'simulate' an applicationState object - /classes/DependencyMaps and this file rely
+//                    // on having these attributes, at a minimum. We just want to display the missing app
+//                    // so this should be OK - but not ideal
+//                    var showAsMissing = {
+//                        'componentId': path,
+//                        'configurationPath': path,
+//                        'applicationStatusClass': constants.glyphs.unknownQMark,
+//                        'applicationStatusBg': constants.colors.unknownGray,
+//                        'applicationStatus': ko.observable(constants.applicationStatuses.unknown),
+//                        'predType': predType,
+//                        'requires': ko.observableArray([]),
+//                        'errorState': ko.observable(constants.errorStates.unknown)
+//                    };
+//                    self.requires.push({'state': showAsMissing, 'operational': operational});
+//                }
+//            });
         };
 
         self.requirementsAreUp = ko.computed(function() {
-            if (self.requires().length > 0) {
-                for (var i = 0; i < self.requires().length; i++) {
-                    if (self.requires()[i].state.applicationStatus() === constants.applicationStatuses.stopped) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-            else {
-                return true;
-            }
+//            if (self.requires().length > 0) {
+//                for (var i = 0; i < self.requires().length; i++) {
+//                    if (self.requires()[i].state.applicationStatus() === constants.applicationStatuses.stopped) {
+//                        return false;
+//                    }
+//                }
+//                return true;
+//            }
+//            else {
+//                return true;
+//            }
         });
 
         self.requiredBy = ko.computed(function() {
-            var dependencies = ko.observableArray([]);
-            // HATE HATE HATE this double loop. Need to move this processing to the server side.
-            ko.utils.arrayForEach(applicationStateArray(), function(applicationState) {
-                for (var i = 0; i < applicationState.dependencyModel.requires().length; i++) {
-                    if (applicationState.dependencyModel.requires()[i].state == parentAppState) {
-                        dependencies.push(applicationState);
-                        break;
-                    }
-                }
-            });
-
-            dependencies.sort();
-            return dependencies().slice();
+//            var dependencies = ko.observableArray([]);
+//            // HATE HATE HATE this double loop. Need to move this processing to the server side.
+//            ko.utils.arrayForEach(applicationStateArray(), function(applicationState) {
+//                for (var i = 0; i < applicationState.dependencyModel.requires().length; i++) {
+//                    if (applicationState.dependencyModel.requires()[i].state == parentAppState) {
+//                        dependencies.push(applicationState);
+//                        break;
+//                    }
+//                }
+//            });
+//
+//            dependencies.sort();
+//            return dependencies().slice();
         }).extend({rateLimit: 2000});
 
         self.dependencyClass = ko.computed(function() {
-            if (self.requires().length === 0 &&
-                self.holiday().length === 0 &&
-                self.weekend().length === 0 &&
-                self.zookeepergooduntiltime().length === 0) {
-                return '';
-            }
-            else if (self.showDependencies()) {
+//            if (self.requires().length === 0 &&
+//                self.holiday().length === 0 &&
+//                self.weekend().length === 0 &&
+//                self.zookeepergooduntiltime().length === 0) {
+//                return '';
+//            }
+//            else
+            if (self.showDependencies()) {
                 return 'caret';
             }
             else {
@@ -136,12 +138,14 @@ define(['knockout', 'model/constants'], function(ko, constants) {
         });
 
         self.dependencyVisible = ko.computed(function () {
-            return ((
-                    self.requires().length > 0 ||
-                    self.zookeepergooduntiltime().length > 0 ||
-                    self.weekend().length > 0 ||
-                    self.holiday().length > 0
-                    ) && self.showDependencies());
+            return (
+//                (
+//                    self.requires().length > 0 ||
+//                    self.zookeepergooduntiltime().length > 0 ||
+//                    self.weekend().length > 0 ||
+//                    self.holiday().length > 0
+//                    ) &&
+                self.showDependencies());
         });
 
         self.dependencyPointer = ko.computed(function() {
@@ -150,6 +154,16 @@ define(['knockout', 'model/constants'], function(ko, constants) {
             }
             else {
                 return '';
+            }
+        });
+
+        self.getDeps = ko.computed(function() {
+            if (self.showDependencies()) {
+                $.getJSON('/api/application/dependencies' + parentAppState.configurationPath, function(data) {
+                    self.newRequires(data.dependencies);
+                }).fail(function(data) {
+                    swal('Failed GET for getDeps.', JSON.stringify(data), 'error');
+                });
             }
         });
     };
